a94bde724e0914c6cc79626f90e8eb1f02d5d1c7
Tom Prats <tprats108@gmail.com>
2016-08-12 23:55:15 -0400

    Functionality

--- Historical Markers Nearby/ViewController.swift
+++ Historical Markers Nearby/ViewController.swift
@@ -6,20 +6,94 @@
 //  Copyright © 2016 Tomify. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var nearbyButton: UIButton!
+    @IBAction func nearbyButtonClick(_ sender: AnyObject) {
+        UIApplication.shared.openURL(nearbyURL)
+    }
+    
+    var nearbyURL: URL!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nearbyButton.isHidden = true
     }
-
-
+    
+    func viewDidAppear() {
+        checkLocationAuthorization()
+    }
+    
+    func locationManager(_ locationManager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            showLocationButton(latitude: String(latitude), longitude: String(longitude))
+        } else {
+            showLocationError(error: "There was a problem finding your location. Please try again later.")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        showLocationError(error: error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.requestLocation()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            let alertController = UIAlertController(
+                title: "Location Access Disabled",
+                message: "In order to use this app, please open this app's settings and update the location access.",
+                preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+                let url = URL(string:UIApplicationOpenSettingsURLString)
+                UIApplication.shared.openURL(url!)
+            }
+            alertController.addAction(openAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func showLocationButton(latitude: String, longitude: String) {
+        nearbyURL = URL(string: "https://www.hmdb.org/map.asp?nearby=yes&Latitude=\(latitude)&Longitude=\(longitude)")
+        nearbyButton.isHidden = false
+    }
+    
+    func showLocationError(error: String) {
+        let alertController = UIAlertController(
+            title: "Could Not Find Location",
+            message: error,
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
-
